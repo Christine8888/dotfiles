04f144421d057c9af628e10f1962cbf832fc5732
Christine Ye <53855447+Christine8888@users.noreply.github.com>
2025-08-20 22:39:40 -0700

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -24,3 +24,14 @@ cd ..
 
 # 4) Setup github
 echo ./scripts/setup_github.sh "jpl.hughes@btinternet.com" "John Hughes"
+
+# 5) Install various things Christine needs
+uv run python -m ipykernel install --user --name "venv"
+uv pip install dotenv trl transformers torch "huggingface_hub[cli]" wandb dotenv deepspeed inspect-ai
+uv pip install vllm --torch-backend=auto
+export HF_HOME="/workspace/.cache/huggingface"
+cd /workspace/rl-character/safety-tooling
+uv pip install -e .
+uv pip install git+https://github.com/UKGovernmentBEIS/inspect_evals
+uv pip install git+https://github.com/josejg/instruction_following_eval.git
+uv pip install --upgrade openai anthropic

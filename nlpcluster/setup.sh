f37d049abe21ef30ad064f227238f5d7cf34b4f1
Christine Ye <53855447+Christine8888@users.noreply.github.com>
2025-10-08 13:39:16 -0700

    Create setup.sh

---
+++ nlpcluster/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCR=/scr-ssd/cye
+
+export UV_CACHE_DIR="$SCR/.cache/uv"
+uv python install 3.11
+uv venv --python 3.11
+source .venv/bin/activate
+
+uv pip install dotenv trl transformers torch "huggingface_hub[cli]" wandb dotenv deepspeed
+uv pip install vllm --torch-backend=auto
+export HF_HOME="$NLP/.cache/huggingface"
+cd $NLP/emergent-doordash
+uv pip install -e .
+uv pip install inspect-ai
+uv pip install --upgrade openai anthropic transformers
+uv pip install numpy==2.2
